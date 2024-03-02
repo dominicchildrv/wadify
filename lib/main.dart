fbec5892cddb954500ea8884621995dfeec4eabe
dominicchildrv <114876361+dominicchildrv@users.noreply.github.com>
2024-03-02 14:33:54 +0000

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:test_app/pages/home_page.dart';
 import 'pages/start_page.dart';
+import 'pages/home_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(MyApp(token: prefs.getString('token')));
 }
 
 class MyApp extends StatelessWidget {
+
+  final token;
+
+  const MyApp({
+    @required this.token,
+    Key? key,
+
+}): super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,8 +29,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      //home: (JwtDecoder.isExpired(token) == false)?HomePage(token: token):StartPage(),
       home: StartPage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
+
